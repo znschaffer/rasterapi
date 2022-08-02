95a50c41311920eeacc47ecdfa86fc85e25fc9f5
Zane Schaffer <personal.zane@gmail.com>
2022-08-02 12:48:39 -0700

    fix: change env variable name

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := App{}
 	app.Initialize(
 		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
